f77b6d50f61ea78548ffc0f414a8437a9b6803f1
maxinelobato <barretolobato.org@gmail.com>
2023-10-03 16:46:27 -0300

    changes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen items-center justify-center p-24">
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-orange-600 after:dark:via-orange-400 after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/vercel.svg"
+          alt="Vercel.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+    </main>
+  );
+}
